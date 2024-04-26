99654982a7a54017a92e10e65947c4f738cc08a0
vinq <vinq@ADMIN79>
2024-04-26 16:11:16 +0700

    Add project files.

---
+++ LC242E_Anagram/AnagramChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC242E_Anagram
+{
+    public class AnagramChecker
+    {
+        public bool AreAnagrams(string str1, string str2)
+        {
+            if (str1.Length != str2.Length) return false;
+
+            char[] charArrayStr1 = str1.ToCharArray();
+            char[] charArrayStr2 = str2.ToCharArray();
+            Array.Sort(charArrayStr1);
+            Array.Sort(charArrayStr2);
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (charArrayStr1[i] != charArrayStr2[i]) 
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LC242E_Anagram/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LC242E_Anagram;
+
+string str1 = "listen";
+string str2 = "silent";
+
+AnagramChecker anagramChecker = new AnagramChecker();
+
+if (anagramChecker.AreAnagrams(str1, str2))
+    Console.WriteLine("The strings are anagrams");
+else
+    Console.WriteLine("The strings are not anagrams");
